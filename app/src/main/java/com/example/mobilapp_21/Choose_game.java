6a2b9450c8b611d4ebbaa810736a11c9914e1d98
fredrikmejs <s185130@student.dtu.dk>
2019-11-09 20:25:27 +0100

    main menu setup done

--- app/src/main/java/com/example/mobilapp_21/Choose_game.java
+++ app/src/main/java/com/example/mobilapp_21/Choose_game.java
@@ -2,13 +2,52 @@ package com.example.mobilapp_21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Choose_game extends AppCompatActivity {
+public class Choose_game extends AppCompatActivity implements View.OnClickListener  {
+
+    private Button button_howToPlay, button_RegneArk, button_DR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_game);
+
+
+        button_DR = findViewById(R.id.button_DROrd);
+        button_DR.setOnClickListener(this);
+
+        button_howToPlay = findViewById(R.id.button_HowToPlay);
+        button_howToPlay.setOnClickListener(this);
+
+        button_RegneArk = findViewById(R.id.button_regneArk);
+        button_RegneArk.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == button_DR){
+            Intent intent = new Intent(this, GalgeSpil.class);
+            intent.putExtra("GameType",0);
+            startActivity(intent);
+
+        }
+
+        if (v == button_howToPlay) {
+            Intent intent = new Intent(this, HowToPlay.class);
+            startActivity(intent);
+
+        }
+
+        if (v == button_RegneArk) {
+            Intent intent = new Intent(this,Difficulty.class);
+            startActivity(intent);
+
+        }
     }
 }
